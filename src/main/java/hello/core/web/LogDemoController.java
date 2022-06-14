5ad5c1b07a33eb1324d65d590e61fca2fdbf5403
DanDChoi <showbegin622@gmail.com>
2022-06-14 14:50:24 +0900

    scope and proxy

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -14,16 +14,16 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request ){
+    public String logDemo(HttpServletRequest request ) throws InterruptedException {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("Controller test");
+        Thread.sleep(1000);
         logDemoService.logic("testID");
         return "OK";
     }
