1de4d56f13bc2e99495354993692993535367f40
DanDChoi <showbegin622@gmail.com>
2022-09-26 17:28:25 +0900

    controller v2

---
+++ src/main/java/hello/upload/controller/ServletUploadControllerV2.java
@@ -0,0 +1,12 @@
+package hello.upload.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/servlet/v2")
+public class ServletUploadControllerV2 {
+
+}
